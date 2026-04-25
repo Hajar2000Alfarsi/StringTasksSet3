cbfcda7d3e3cb08ba347031184d1ca03b7936a82
Hajar <hajar2000alfarsi@gmail.com>
2026-04-25 19:22:27 +0400

    add task 8: Find mirror ends substring

--- src/EndsMirror.java
+++ src/EndsMirror.java
@@ -1,11 +1,15 @@
 public class EndsMirror {
     public static void main(String[] args) {
+        EndsMirror tk8= new EndsMirror();
 
+        System.out.println(tk8.mirrorEnds("abXYZba"));
+        System.out.println(tk8.mirrorEnds("abca"));
+        System.out.println(tk8.mirrorEnds("aba"));
     }
     public String mirrorEnds(String inputText) {
             String resultText= "";
-            for (int i = 0; i <= inputText.length() / 2; i++) {
-                if (inputText.charAt(i) == inputText.charAt(inputText.length()) - 1) {
+            for (int i = 0; i < inputText.length(); i++) {
+                if (inputText.charAt(i) == inputText.charAt(inputText.length()- 1 - i)) {
                     resultText += inputText.charAt(i);
                 } else {
                     break;
