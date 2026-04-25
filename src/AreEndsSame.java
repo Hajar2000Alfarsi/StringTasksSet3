0ed152f0e51299fe1458ba86a74a95005a56f15d
Hajar <hajar2000alfarsi@gmail.com>
2026-04-25 18:37:14 +0400

    add task 7: Find longest matching start and end substring

--- src/AreEndsSame.java
+++ src/AreEndsSame.java
@@ -1,16 +1,20 @@
 public class AreEndsSame {
     public static void main(String[] args) {
+        AreEndsSame tk7= new AreEndsSame();
 
+        System.out.println(tk7.sameEnds("abXYab"));
+        System.out.println(tk7.sameEnds("xxHelloxx"));
+        System.out.println(tk7.sameEnds("xxx"));
     }
     public String sameEnds(String inputText) {
         String sameStartEnd= "";
 
-        for (int i = 0; i < inputText.length() / 2; i++){
+        for (int i = 0; i <= inputText.length() / 2; i++){
             String startPart = inputText.substring(0,i);
             String endPart = inputText.substring(inputText.length() - i);
 
             if(startPart.equals(endPart)){
-                sameStartEnd += startPart;
+                sameStartEnd = startPart;
             }
         }
         return sameStartEnd;
