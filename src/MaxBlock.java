9ce4b10ce68d0508989824be8cc32320f6e1687d
Hajar <hajar2000alfarsi@gmail.com>
2026-04-25 19:44:03 +0400

    add task 9: Find largest block of repeating characters

--- src/MaxBlock.java
+++ src/MaxBlock.java
@@ -1,22 +1,25 @@
 public class MaxBlock {
     public static void main(String[] args){
-
+        MaxBlock tk9 = new MaxBlock();
+        System.out.println(tk9.maxBlock("hoopla"));
+        System.out.println(tk9.maxBlock("abbCCCddBBBxx"));
+        System.out.println(tk9.maxBlock(""));
     }
     public int maxBlock(String inputText) {
-        int currentBlock= 0;
+        int currentBlock= 1;
         int maxBlock = 0;
         if (inputText.length() == 0){
             return 0;
         }
-        for (int i = 0; i < inputText.length(); i++) {
+        for (int i = 0; i < inputText.length() -1; i++) {
             if (inputText.charAt(i) == inputText.charAt(i+1)){
                 currentBlock ++;
             } else {
-                currentBlock =0;
+                currentBlock =1;
             }
-        }
-        if (maxBlock < currentBlock) {
-            maxBlock= currentBlock;
+            if (maxBlock < currentBlock) {
+                maxBlock= currentBlock;
+            }
         }
         return maxBlock;
     }
