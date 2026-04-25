7f1bd0fb6ae841c69d1f55422ea17630a4a167e2
Hajar <hajar2000alfarsi@gmail.com>
2026-04-25 20:09:19 +0400

    add task 10: Sum full numbers in string

--- src/SumOfNumbers.java
+++ src/SumOfNumbers.java
@@ -1,22 +1,23 @@
 public class SumOfNumbers {
     public static void main(String[] args) {
-
+        SumOfNumbers tk10= new SumOfNumbers();
+        System.out.println(tk10.sumNumbers("abc123xyz"));
+        System.out.println(tk10.sumNumbers("aa11b33"));
+        System.out.println(tk10.sumNumbers("7 11"));
     }
     public int sumNumbers(String inputText) {
         int sum= 0;
         int numToAdd = 0;
-        int digit = 1;
         for (int i = 0; i < inputText.length(); i++){
             char currentChar = inputText.charAt(i);
             if (Character.isDigit(currentChar)) {
-                numToAdd = numToAdd + digit * (currentChar - '0');
-                digit *= 10;
+                numToAdd = numToAdd  * 10 + (currentChar - '0');
             } else {
+                sum += numToAdd;
                 numToAdd = 0;
-                digit = 1;
             }
-            sum += numToAdd;
         }
+        sum += numToAdd;
         return sum;
     }
 }
