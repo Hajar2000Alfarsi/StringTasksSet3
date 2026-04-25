7aa8a1b817b04f0e10c49f0c842ddcfacc866a7a
Hajar <hajar2000alfarsi@gmail.com>
2026-04-25 19:17:25 +0400

    task 8:Find mirror ends substring

--- src/EndsMirror.java
+++ src/EndsMirror.java
@@ -3,6 +3,14 @@ public class EndsMirror {
 
     }
     public String mirrorEnds(String inputText) {
-
+            String resultText= "";
+            for (int i = 0; i <= inputText.length() / 2; i++) {
+                if (inputText.charAt(i) == inputText.charAt(inputText.length()) - 1) {
+                    resultText += inputText.charAt(i);
+                } else {
+                    break;
+                }
+            }
+            return resultText;
     }
 }
