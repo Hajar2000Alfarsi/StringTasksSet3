fcc5357da690ebe3aa9ec3dd56aa05f853dba851
Hajar <hajar2000alfarsi@gmail.com>
2026-04-25 20:38:38 +0400

    add task 11: replace is to is not

--- src/ReplaceIs.java
+++ src/ReplaceIs.java
@@ -1,11 +1,15 @@
 public class ReplaceIs {
     public static void main(String[] args) {
+        ReplaceIs tk11= new ReplaceIs();
 
+        System.out.println(tk11.notReplace("is test"));
+        System.out.println(tk11.notReplace("is-is"));
+        System.out.println(tk11.notReplace("This is right"));
     }
     public String notReplace(String inputText) {
         String resultText = "";
-        for (int i = 0; i > inputText.length() - 1; i++){
-            if(inputText.substring(i, i+2).equals("is")){
+        for (int i = 0; i < inputText.length(); i++){
+            if(i <= inputText.length()-2 && inputText.substring(i, i+2).equals("is")){
                 boolean beforeIs = (i > 0 && Character.isLetter(inputText.charAt(i-1)));
                 boolean afterIs = (i < inputText.length() -2 && Character.isLetter(inputText.charAt(i+2)));
                 if (!beforeIs && !afterIs){
